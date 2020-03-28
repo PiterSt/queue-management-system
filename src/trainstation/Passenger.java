f8ceb9b19a62b08674a501fb9610e0660a40726e
Piotr Stanny <piotr.stanny@gmail.com>
2020-03-28 18:02:57 +0000

    Passenger class getters and setters added

--- src/trainstation/Passenger.java
+++ src/trainstation/Passenger.java
@@ -4,5 +4,30 @@ package trainstation;
  * @author piotrstanny
  */
 public class Passenger {
+    String firstName, surname;
+    int secondsInQueue;
     
+    Passenger(String firstName, String surname){
+        this.firstName = firstName;
+        this.surname = surname;
+        secondsInQueue = 0;
+    }
+    
+    public String getName(){
+        return firstName + " " + surname;
+    }
+    public void setName(String firstName, String surname){
+        this.firstName = firstName;
+        this.surname = surname;
+    }
+    public int getSeconds(){
+        return secondsInQueue;
+    }
+    public void setSecondsInQueue(int seconds){
+        this.secondsInQueue = seconds;
+    }
+    
+    public void display(){
+        System.out.println("Passenger details: " + firstName + " " + surname + "Seconds in a queue: " + secondsInQueue);
+    }
 }
